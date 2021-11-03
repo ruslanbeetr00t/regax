1660852c488018bee4c823f6f87f4e6baef771c1
rusya <treshmetal.ua@gmail.com>
2021-11-03 16:19:18 +0200

    second_try

--- regax/__init__.py
+++ regax/__init__.py
@@ -1,14 +1,15 @@
 import re
 
 def try_use_regax():
-    number = input('add phone')
-    phone_number = re.compile(r'\d\d\d\d\d\d\d\d\d\d')
-    my = phone_number.search(f'My phone number: {number}')
-    print('phone:' + my.group())
-    print(type(my))
-    if len(str(my.group())) == 10:
-        print('ok')
-    else:
-        return try_use_regax()
+    try:
+        number = input('add phone')
+        phone_number = re.compile(r'\d\d\d\d\d\d\d\d\d\d')
+        my = phone_number.search(f'My phone number: {number}')
+        print('phone:' + my.group())
+        print(type(my))
+        if len(str(my.group())) == 10:
+            print('ok')
+    except TypeError:
+        print('Будьте уважнішим')
 
 try_use_regax()
